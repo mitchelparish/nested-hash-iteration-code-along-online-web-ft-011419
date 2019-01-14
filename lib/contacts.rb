a6293aea0cfe5aa3f8befbd4a3c17c2b05029c9e
Mitchel Parish <mitchelparish@gmail.com>
2019-01-14 23:58:22 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -17,9 +17,10 @@ require 'pry'
 def remove_strawberry(contacts)
   contacts.each do |person, contact_details_hash|
     contact_details_hash.each do |attribute, data|
-      binding.pry 
+      binding.pry
 
 
 
-end
-end
+    end
+  end
+end 
